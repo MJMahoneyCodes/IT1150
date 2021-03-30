b52bf7c85150e1e05b3cdcc04a7deaf1c15c09ef
MJMahoneyCodes <ziggurattt@gmail.com>
2021-03-30 19:25:37 -0400

    Add files via upload

---
+++ LessonFour/scripts.js
@@ -0,0 +1,9 @@
+//used in the responsive top nav bar
+function openMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
