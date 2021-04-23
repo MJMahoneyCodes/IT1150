1a10cfc9a20f846d36f810c8753462f474867205
MJMahoneyCodes <ziggurattt@gmail.com>
2021-04-22 20:49:53 -0400

    made lots of changes

--- LessonFive/scripts.js
+++ LessonFive/scripts.js
@@ -9,17 +9,26 @@
                 $(".IETF").show();
                 $(".ICANN").hide();
                 $(".WWWC").hide();
+                $("#IETF").addClass("active");
+                $("#ICANN").removeClass("active");
+                $("#WWWC").removeClass("active");
             });
             //clicking on the element with id=ICANN shows ICANN class' elements and hides the others'
             $("#ICANN").click(function(){
                 $(".ICANN").show();
                 $(".IETF").hide();
                 $(".WWWC").hide();
+                $("#ICANN").addClass("active");
+                $("#IETF").removeClass("active");
+                $("#WWWC").removeClass("active");
             });
             //clicking on the element with id=WWWC shows the WWWC class' elements and hides the others'
             $("#WWWC").click(function(){
                 $(".WWWC").show();
                 $(".IETF").hide();
                 $(".ICANN").hide();
+                $("#WWWC").addClass("active");
+                $("#ICANN").removeClass("active");
+                $("#IETF").removeClass("active");
             });
         });
